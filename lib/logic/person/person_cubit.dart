cfde678efe946778f2e819f3df343a7ffa643bc6
akhmadjonakbarov <akhmadjonakbarov@gmail.com>
2022-11-26 16:30:53 +0500

    crud functions was done

--- lib/logic/person/person_cubit.dart
+++ lib/logic/person/person_cubit.dart
@@ -1,5 +1,7 @@
 import 'package:bloc/bloc.dart';
-import 'package:kirim_chiqim/models/person.dart';
+import 'package:flutter/foundation.dart';
+import '../../database/person_operations.dart';
+import '../../models/person.dart';
 import 'package:meta/meta.dart';
 
 part 'person_state.dart';
@@ -9,19 +11,76 @@ class PersonCubit extends Cubit<PersonState> {
     emit(PersonWelcome());
   }
 
-  void addPerson() {
-    try {} catch (e) {}
+  void addPerson({String? name, String? phoneNumber}) {
+    List<Person> persons = [];
+    if (state.persons == null || state.persons!.isEmpty) {
+      persons = [];
+    } else {
+      persons = state.persons!;
+    }
+    try {
+      emit(PersonWelcome());
+      Person newPerson = Person(
+        id: UniqueKey().toString(),
+        name: name!,
+        phoneNumber: phoneNumber!,
+      );
+      persons.add(newPerson);
+      emit(PersonLoaded(personsLoaded: persons));
+      PersonOperations.insertPersons(data: newPerson.toMap());
+    } catch (e) {
+      emit(
+        PersonError(
+          errorMsg: e.toString(),
+        ),
+      );
+    }
   }
 
-  void updatePerson() {
-    try {} catch (e) {}
+  void updatePerson({Person? person}) {
+    try {
+      final persons = state.persons!.map((p) {
+        if (p.id == person!.id) {
+          return Person(
+            id: person.id,
+            name: person.name,
+            phoneNumber: person.phoneNumber,
+          );
+        }
+        return p;
+      }).toList();
+      emit(PersonLoading());
+      emit(PersonLoaded(personsLoaded: persons));
+      PersonOperations.updatePerson(person: person);
+    } catch (e) {
+      emit(
+        PersonError(errorMsg: e.toString()),
+      );
+    }
   }
 
-  void deletePerson() {
-    try {} catch (e) {}
+  void deletePerson({String? personId}) {
+    List<Person> persons = state.persons!;
+    try {
+      persons.removeWhere((element) => element.id == personId);
+      emit(PersonDelete(personsDeleted: persons));
+      emit(PersonLoading());
+      emit(PersonLoaded(personsLoaded: persons));
+      PersonOperations.deletePerson(personId: personId);
+    } catch (e) {
+      emit(PersonError(
+        errorMsg: e.toString(),
+      ));
+    }
   }
 
   List<Person> searchPersons(String name) {
-    return [];
+    return state.persons!
+        .where(
+          (element) => element.name.toLowerCase().contains(
+                name.toLowerCase(),
+              ),
+        )
+        .toList();
   }
 }
