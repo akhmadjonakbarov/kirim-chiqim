ae37305fba378cf1be66e4e6077956b584086a9a
akhmadjonakbarov <akhmadjonakbarov@gmail.com>
2022-11-27 23:52:51 +0500

    searchbar was done

---
+++ lib/screens/home/widgets/search_bar.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/rendering/sliver_grid.dart';
+import 'package:flutter/src/rendering/sliver.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:kirim_chiqim/logic/models/person.dart';
+import 'package:kirim_chiqim/logic/person/person_cubit.dart';
+import 'package:kirim_chiqim/screens/detail/detail_screen.dart';
+
+class SearchBar extends SearchDelegate {
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      TextButton(
+        onPressed: () => query = '',
+        child: const Text("Tozalash"),
+      )
+    ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+      onPressed: () => close(context, null),
+      icon: Icon(Icons.arrow_back),
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    final persons = BlocProvider.of<PersonCubit>(context).searchPersons(query);
+    return ListView.builder(
+      itemCount: persons.length,
+      itemBuilder: (context, index) {
+        Person person = persons[index];
+        return ListTile(
+          title: Text(person.name),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    if (query.isNotEmpty) {
+      final persons =
+          BlocProvider.of<PersonCubit>(context).searchPersons(query);
+      if (persons.isEmpty) {
+        return const Text("Bunday note mavjud emas!");
+      }
+      return ListView.builder(
+        itemCount: persons.length,
+        itemBuilder: (context, index) {
+          Person person = persons[index];
+          return ListTile(
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) {
+                    return DetailScreen(
+                      person: person,
+                    );
+                  },
+                ),
+              );
+            },
+            title: Text(person.name),
+          );
+        },
+      );
+    }
+    return Container();
+  }
+}
